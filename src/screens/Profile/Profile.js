996c3e9b182933630b48a5602fb81da978108f69
“RJ”-CodeByte <jay.imscit17@gmail.com>
2022-05-30 14:31:12 +0530

    as below also in profile

--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -1,11 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+import React, { useEffect, useState } from 'react'
 
-const Profile = () => {
+const Profile = ({ navigation, route, setParams }) => {
+    const { data } = route.params
+
     return (
-        <View>
-            <Text>Profile</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: 'center' }}>
+            {route.params.data ?
+                <Text>{data.name}</Text> : <Text>Profile</Text>}
+
+        </SafeAreaView>
     )
 }
 
