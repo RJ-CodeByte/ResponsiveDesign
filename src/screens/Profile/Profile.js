cf06448ea1073f7e12989ac0f4644961b7709d87
“RJ”-CodeByte <jay.imscit17@gmail.com>
2022-06-02 18:54:27 +0530

    headerWith curve

--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -7,15 +7,29 @@ const Profile = ({ navigation, route, setParams }) => {
     const [count, setcount] = useState(0)
 
     return (
-        <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: 'center' }}>
+
+        <SafeAreaView style={{
+            flex: 1,
+            backgroundColor: "#009387"
+        }}>
+            <View style={{ flex: 0.1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>headers</Text>
+            </View>
+            <View style={{
+                flex: 1, backgroundColor: '#fff', borderTopLeftRadius: 30, borderTopRightRadius: 30,
+                paddingVertical: 50, paddingHorizontal: 30
+            }}>
+                <Text>headers</Text>
+            </View>
+
             {/* {route.params.data ?
                 <Text>{data.name}</Text>
 
                 : <Text>Profile</Text>} */}
-            <View>
+            {/* <View>
                 <Text>{count}</Text>
                 <IncrementBtn count={count} incrementCount={(count) => { setcount(count) }} />
-            </View>
+            </View> */}
         </SafeAreaView>
     )
 }
